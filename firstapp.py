db4385b3b09e96db81399c44f5bdeb18486c9283
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-11-09 23:00:02 -0500

    Add files via upload

---
+++ firstapp.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 20:56:34 2020
+
+@author: amirmohammadbehdani
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import os
+import joblib 
+
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
+import hashlib
+
+from managedb import *
+
+# Password 
+def generate_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+def verify_hashes(password,hashed_text):
+	if generate_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+
+
+def main():
+
+
+    st.title('Diabetes Analysis App')
+    st.subheader('VCU School of Pharmacy')
+    
+    menu = ["Home","Login","SignUp"]
+    submenu = [ "Plot" , "Prediction", "Analytics" ]
+    
+    choice = st.sidebar.selectbox ("Menu", menu)
+    if choice == "Home":
+        st.subheader("Home")
+        st.text( "What is Diabetes?")
+    elif choice == "Login" :
+        username = st.sidebar.text_input ("Username")
+        password = st.sidebar.text_input ("password", type='password')
+        if st.sidebar.checkbox("Login"):
+            create_usertable()
+            hashed_pswd = generate_hashes(password)
+            result = login_user(username,verify_hashes(password,hashed_pswd))
+
+            if result:
+                    st.success("Welcome {}".format(username))
+                    activity = st.selectbox("Activity", submenu)
+                    if activity == ( "Plot"):    
+                        st.subheader("Data Visualization Plot")
+                        df = pd.read_csv("data/Diabetesdata.csv")
+                        st.dataframe(df)
+                        
+                        df['Total - Percentage'].value_counts().plot(kind='bar')
+                        st.pyplot()
+                        
+                        
+                    elif activity == "Prediction":
+                            st.subheader("Predictive Analytics")
+        else:
+    	        st.warning("Incorrect Username/Password, Please try again")
+
+    elif choice == "SignUp" :
+        new_username = st.text_input("User name")
+        new_password = st.text_input("Password", type='password')
+
+        confirm_password = st.text_input("Confirm Password", type='password')
+        if new_password == confirm_password:
+            st.success("Password Confirmed")
+        else:
+            st.warning("Passwords not the same")
+            
+        if st.button("Submit"):
+             create_usertable()
+             hashed_new_password = generate_hashes(new_password)
+             add_userdata(new_username,hashed_new_password)
+             st.success("You have successfully created a new account")
+             st.info("Login to Get Started")
+ 
+        
+
+
+
+
+
+
+
+
+if __name__ == '__main__' :
+    main()
+    
+    st.set_option('deprecation.showPyplotGlobalUse', False)
