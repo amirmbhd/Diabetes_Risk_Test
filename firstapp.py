f9b7b2439f58cc94858b3679c8439ade8dd2db8a
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-12-15 22:47:43 -0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -94,11 +94,11 @@ def main():
                         df['class'].value_counts().plot(kind='bar')
                         st.pyplot()
                         
-		    if st.checkbox("Area Chart"):
-				all_columns = df.columns.to_list()
-				feat_choices = st.multiselect("Choose a Feature",all_columns)
-				new_df = df[feat_choices]
-				st.area_chart(new_df)
+		    	if st.checkbox("Area Chart"):
+			all_columns = df.columns.to_list()
+			feat_choices = st.multiselect("Choose a Feature",all_columns)
+			new_df = df[feat_choices]
+			st.area_chart(new_df)
 				
                     elif activity == "Prediction":
                             st.subheader("Predictive Analytics")
