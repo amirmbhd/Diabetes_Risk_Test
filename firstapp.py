ebcf9bfa6f565b241a690945485397dc36a975c6
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-12-15 22:19:05 -0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -36,7 +36,7 @@ def verify_hashes(password,hashed_text):
 
 def load_images(image_name):
     img = Image.open(image_name)
-    return st.image(img, width=400)
+    return st.image(img, width=800)
 
 gender_dict = {"male":1,"female":2}
 feature_dict = {"No":1,"Yes":2}
