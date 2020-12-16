ec80fdbc3ee54cce9fa3bad43bee5de33a01b10e
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-12-16 15:00:00 -0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -131,11 +131,11 @@ def main():
                             model_choice = st.selectbox("Select Model",["LR","KNN","DecisionTree"])
                             if st.button("Predict"):
                                     if model_choice == "KNN":
-                                            loaded_model = load_model("models/knn_hepB_model.pkl")
+                                            loaded_model = load_model("models/knn_diabetes_model.pkl")
                                             prediction = loaded_model.predict(single_sample)
                                             pred_prob = loaded_model.predict_proba(single_sample)
                                     elif model_choice == "DecisionTree":
-                                            loaded_model = load_model("models/decision_tree_clf_hepB_model.pkl")
+                                            loaded_model = load_model("models/decision_tree_clf_diabetes_model.pkl")
                                             prediction = loaded_model.predict(single_sample)
                                             pred_prob = loaded_model.predict_proba(single_sample)
                                     else:
