47d994a5b9398a3a836032c2740297beee831958
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-12-16 12:32:19 -0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -108,11 +108,11 @@ def main():
 				
                     elif activity == "Prediction":
                             st.subheader("Predictive Analytics")
-                            age = st.number_input("Age",7,80)
-                            sex = st.radio("Sex",tuple(gender_dict.keys()))
-                            history = st.radio("Do you have a mother, father, sister, or brother with diabetes??",tuple(feature_dict.keys()))
-                            bp = st.radio("Have you ever been diagnosed with high blood pressure?",tuple(feature_dict.keys()))
-                            activity = st.radio("Are you physically active?",tuple(feature_dict.keys()))
+                            Age = st.number_input("Age",7,80)
+                            Sex = st.radio("Sex",tuple(gender_dict.keys()))
+                            Polyuria = st.radio("Do you have Polyuria?",tuple(feature_dict.keys()))
+                            sudden weight loss = st.radio("Hve you recently had sudden weight loss?",tuple(feature_dict.keys()))
+                            weakness = st.radio("Do you usually have muscle weakness?",tuple(feature_dict.keys()))
 
 
 
