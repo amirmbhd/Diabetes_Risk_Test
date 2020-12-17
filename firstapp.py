bca8c18b42a70ec5ec7abf22c3ada9fe5f396e03
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-12-16 22:26:27 -0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -175,20 +175,7 @@ def main():
                                         feature_names = ['Age', 'Gender', 'Polyuria', 'Polydipsia', 'Sudden_weight_loss', 'Weakness', 'Polyphagia', 'Genital thrush', 'Visual_blurring','Itching', 'Irritability', 'Delayed_healing', 'Partial_paresis','Muscle_stiffness', 'Alopecia', 'Obesity']
                                         class_names = ['Negative','Positive']
                                         explainer = lime.lime_tabular.LimeTabularExplainer(x.values,feature_names=feature_names, class_names=class_names,discretize_continuous=True)
-                                        # The Explainer Instance
-                                        exp = explainer.explain_instance(np.array(feature_list), loaded_model.predict_proba,num_features=14, top_labels=1)
-                                        exp.show_in_notebook(show_table=True, show_all=False)
-                                        # exp.save_to_file('lime_oi.html')
-                                        st.write(exp.as_list())
-                                        new_exp = exp.as_list()
-                                        label_limits = [i[0] for i in new_exp]
-                                        # st.write(label_limits)
-                                        label_scores = [i[1] for i in new_exp]
-                                        plt.barh(label_limits,label_scores)
-                                        st.pyplot()
-                                        plt.figure(figsize=(20,10))
-                                        fig = exp.as_pyplot_figure()
-                                        st.pyplot()	
+                                        
 			
 			
 					
