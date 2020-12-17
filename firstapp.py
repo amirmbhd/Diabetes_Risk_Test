4960862e97a603e0889dcd0ce5ab78f0e45133e8
amirmbhd <74167431+amirmbhd@users.noreply.github.com>
2020-12-16 22:48:13 -0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -172,7 +172,7 @@ def main():
 
                                         df = pd.read_csv("data/diabetesdata.csv")
                                         x = df[[ 'Gender', 'Polyuria', 'Polydipsia', 'sudden weight loss', 'weakness', 'Polyphagia', 'Genital thrush', 'visual blurring','Itching', 'Irritability', 'delayed healing', 'partial paresis','muscle stiffness', 'Alopecia', 'Obesity']]
-                                        feature_names = [' 'Gender', 'Polyuria', 'Polydipsia', 'Sudden_weight_loss', 'Weakness', 'Polyphagia', 'Genital thrush', 'Visual_blurring','Itching', 'Irritability', 'Delayed_healing', 'Partial_paresis','Muscle_stiffness', 'Alopecia', 'Obesity']
+                                        feature_names = ['Gender', 'Polyuria', 'Polydipsia', 'Sudden_weight_loss', 'Weakness', 'Polyphagia', 'Genital thrush', 'Visual_blurring','Itching', 'Irritability', 'Delayed_healing', 'Partial_paresis','Muscle_stiffness', 'Alopecia', 'Obesity']
                                         class_names = ['Negative','Positive']
                                         explainer = lime.lime_tabular.LimeTabularExplainer(x.values,feature_names=feature_names, class_names=class_names,discretize_continuous=True)
                                         exp = explainer.explain_instance(np.array(feature_list), loaded_model.predict_proba,num_features=14, top_labels=1)
